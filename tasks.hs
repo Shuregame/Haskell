66faeb0aff7d99058cf2aed001068a44a87512f0
Shuregame <40614611+Shuregame@users.noreply.github.com>
2020-05-27 01:17:21 +0300

    Update tasks.hs

--- tasks.hs
+++ tasks.hs
@@ -1,1 +1,11 @@
 ; Билялов Шевкет,"списки: [1, 2, 3, 15, 22], коды: [4], деревья: [1]
+
+-- 22. Оределите функцию ОБЪЕДИНЕНИЕ, формирующую объединение двух множеств.
+
+combination s1 s2 = s1 ++ (filter (\ x -> not (x `elem` s1)) s2) 
+
+main = do
+    print "Test 1"
+    print $ combination [1, 2] [1]
+    print "Test 2"
+    print $ combination [1, 2] [3]
