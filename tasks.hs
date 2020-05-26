60e635c08acfd052fa64815f877a684285f1dd8b
Shuregame <40614611+Shuregame@users.noreply.github.com>
2020-05-27 01:18:18 +0300

    Update tasks.hs

--- tasks.hs
+++ tasks.hs
@@ -2,10 +2,10 @@
 
 -- 22. Оределите функцию ОБЪЕДИНЕНИЕ, формирующую объединение двух множеств.
 
-combination1 s1 s2 = s1 ++ (filter (\ x -> not (x `elem` s1)) s2) 
+combination s1 s2 = s1 ++ (filter (\ x -> not (x `elem` s1)) s2) 
 
 main = do
     print "Test 1"
-    print $ combination1 [1, 2] [1]
+    print $ combination [1, 2] [1]
     print "Test 2"
-    print $ combination1 [1, 2] [3]
+    print $ combination [1, 2] [3]
